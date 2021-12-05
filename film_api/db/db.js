8c1fcfaaa4b0e371a51b65c033ffead00ff38a08
steve zhang <871506263@qq.com>
2021-12-05 18:35:56 +0800

    修改sql

--- film_api/db/db.js
+++ film_api/db/db.js
@@ -3,8 +3,8 @@ const mysql = require('mysql');
 const conn = mysql.createConnection({
    host:'localhost',    //数据库地址
    user:'root', //用户名
-   password:'123456', //密码
-   database:'db_film' //数据库名
+   password:'881216', //密码
+   database:'zjh' //数据库名
 });
 conn.connect();
 module.exports = conn;
